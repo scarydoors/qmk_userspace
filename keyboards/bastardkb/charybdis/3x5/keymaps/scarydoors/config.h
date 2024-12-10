c260673bca47193caf5a56190f656c87227cd697
Alan Strauhs <alan.strauhs@pagabogroup.com>
2024-12-10 10:39:51 +0000

    feat: enable nkro

--- keyboards/bastardkb/charybdis/3x5/keymaps/scarydoors/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/scarydoors/config.h
@@ -39,3 +39,4 @@
 #define TAPPING_TERM 175
 // #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 0
+#define FORCE_NKRO
--- keyboards/bastardkb/charybdis/3x5/keymaps/scarydoors/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/scarydoors/rules.mk
@@ -3,3 +3,4 @@ SRC += features/layer_lock.c
 VIA_ENABLE = yes
 COMBO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+NKRO_ENABLE = yes
